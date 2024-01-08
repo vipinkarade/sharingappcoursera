dc6b0ba456436534a90f710297a4959ca78c4e41
Vipin Karade <vkarade+eGain@egain.com>
2024-01-08 11:16:48 +0530

    Commit for ContactList

--- app/src/main/java/com/example/sharingapp/ContactList.java
+++ app/src/main/java/com/example/sharingapp/ContactList.java
@@ -1,14 +1,23 @@
 package com.example.sharingapp;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 import android.content.Context;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class ContactList
 {
 	private ArrayList<Contact> contacts;
-	private String FILENAME = "ContactList.java";
+	private String FILENAME = "contacts.sav";
 
 	public void setContacts(ArrayList<Contact> contacts)
 	{
@@ -57,17 +66,46 @@ public class ContactList
 
 	public Contact getContactByUsername(String username)
 	{
-		return contacts.stream().filter(contact -> contact.getUsername().equals(username)).findFirst().get();
+		for(Contact contact : contacts)
+		{
+			if(username.equals(contact.getUsername()))
+			{
+				return contact;
+			}
+		}
+		return null;
 	}
 
 	public void loadContacts(Context context)
 	{
-
+		try {
+            FileInputStream fis = context.openFileInput(FILENAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            Gson gson = new Gson();
+            Type listType = new TypeToken<ArrayList<Contact>>() {}.getType();
+            contacts = gson.fromJson(isr, listType); // temporary
+            fis.close();
+        } catch (FileNotFoundException e) {
+            contacts = new ArrayList<Contact>();
+        } catch (IOException e) {
+            contacts = new ArrayList<Contact>();
+        }
 	}
 
 	public void saveContacts(Context context)
 	{
-
+		try {
+			FileOutputStream fos = context.openFileOutput(FILENAME, 0);
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
+			Gson gson = new Gson();
+			gson.toJson(contacts, osw);
+			osw.flush();
+			fos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public boolean isUsernameAvailable(String username)
